4d83dd51513c1be3e116edde1b786dc2dd1131b6
Jesus Marmol <jesusdavid890@hotmail.com>
2022-03-18 00:06:39 -0500

    CRM Project

---
+++ src/paginas/NuevoCliente.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Formulario from '../components/Formulario'
+
+const NuevoCliente = () => {
+
+  const NuevoCliente = {}
+  return (
+    <>
+      <h1 className='font-black text-4xl text-blue-900'>Nuevo Cliente</h1>
+      <p className='mt-3'>Llena los siguientes campos registrar un cliente</p>
+
+      <Formulario 
+        
+      />
+    </>
+  )
+}
+
+export default NuevoCliente
